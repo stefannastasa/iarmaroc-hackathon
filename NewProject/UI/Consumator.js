6416a7b7208cc7d54c5ab9839f15bf6418f503ec
Tudor <tudorcristianmurariu@yahoo.com>
2021-11-20 20:58:36 +0200

    sheesh

--- NewProject/UI/Consumator.js
+++ NewProject/UI/Consumator.js
@@ -16,6 +16,7 @@ import {
     Text,
     useColorScheme,
     View,
+    Image
   } from 'react-native';
 
 
@@ -28,7 +29,18 @@ const styles = StyleSheet.create({
         flex: 1        
     },
     row: {
-        flexDirection: "row"
+        flexDirection: "row",
+        alignItems: 'center',
+        paddingRight: 30,
+        paddingLeft: 30
+    },
+    img: {
+        height: 150,
+        width: 150,
+        paddingLeft: 10
+    },
+    text: {
+        paddingLeft: 30
     }
 })
 
@@ -36,16 +48,28 @@ const Consumator = () => {
     return (
     <View style={styles.background}>
         <View style={styles.row}>
-            <Image source={require('./images/fructe.jpg')}/>
-            <Image source={require('./images/legume.jpg')}/>
+            <Image style={styles.img} source={require('./images/fructe.jpg')}/>
+            <Image style={styles.img}  source={require('./images/legume.jpg')}/>
         </View>
         <View style={styles.row}>
-            <Image source={require('./images/produse_lactate.jpg')}/>
-            <Image source={require('./images/produse_de_panificatie.jpg')}/>
+            <Text style={styles.text}>Fructe</Text>
+            <Text style={styles.text}>Legume</Text>
         </View>
         <View style={styles.row}>
-            <Image source={require('./images/produse_apicole.jpg')}/>
-            <Image source={require('./images/dulceturi_si_siropuri.jpg')}/>
+            <Image style={styles.img}  source={require('./images/produse_lactate.jpg')}/>
+            <Image style={styles.img}  source={require('./images/produse_de_panificatie.jpg')}/>
+        </View>
+        <View style={styles.row}>
+            <Text style={styles.text}>Produse lactate</Text>
+            <Text style={styles.text}>Produse de panificatie</Text>
+        </View>
+        <View style={styles.row}>
+            <Image style={styles.img}  source={require('./images/produse_apicole.jpg')}/>
+            <Image style={styles.img}  source={require('./images/dulceturi_si_siropuri.jpg')}/>
+        </View>
+        <View style={styles.row}>
+            <Text style={styles.text}>Produse apicole</Text>
+            <Text style={styles.text}>Dulceturi si siropuri</Text>
         </View>
     </View>
     );
