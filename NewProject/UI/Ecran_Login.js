b75ba10fa5d2c93925a23a1e0cd0c0704ec71752
Deea <deea.lip@gmail.com>
2021-11-20 19:51:29 +0200

    images

--- NewProject/UI/Ecran_Login.js
+++ NewProject/UI/Ecran_Login.js
@@ -1,29 +1,44 @@
 import React from "react"
-import {StyleSheet, Text, View} from "react-native"
-import App from "../App"
+import {StyleSheet, Text, Image, View, SafeAreaView} from "react-native"
+
+
+
 
 //const LogIn_Screen = {}
 
 const LogIn_Screen = () => {
     return(
-        <View style = {styles.container}>
-            <Text>This is the first screen</Text>
-            
-        </View>
+        <SafeAreaView style={styles.container}>
+            <Text>Hello, this is the first page of the app</Text>
+            <Image source={require("./iarmarocLogo.png")}/>
+        </SafeAreaView>
+
     );
 }
 
+ /*
+        <View style={[styles.container, {
+            // Try setting `flexDirection` to `"row"`.
+            flexDirection: "column"
+          }]}>
+            <Image source={require('./iarmaroc logo.png')}/>
+            
+            <View style={{ flex: 3, backgroundImage: logo }} />
+            <View style={{ flex: 2, backgroundColor: "purple" }} />
+            <View style={{ flex: 1, backgroundColor: "green" }} />
+            
+          </View>
+        */
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColour: "red",
-        alignItems: "center",
-        justifyContent: "center",
+       //backgroundColour: "red",
+        //alignItems: "center",
+        //justifyContent: "center",
+        padding: 20,
     }
 })
-
-
 
 
 export default LogIn_Screen
