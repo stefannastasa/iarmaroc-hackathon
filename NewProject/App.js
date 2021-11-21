6dcc55405fb158d605193918cdb91e2c6c396f18
Deea <deea.lip@gmail.com>
2021-11-21 02:05:00 +0200

    tried smth in app js

--- NewProject/App.js
+++ NewProject/App.js
@@ -19,6 +19,7 @@ import {
   Text,
   useColorScheme,
   View,
+  Button,
 } from 'react-native';
 
 import {
@@ -34,10 +35,41 @@ import Consumator from './UI/Consumator.js'
 import LogIn_Screen from './UI/Ecran_Login.js';
 
 
+
+/*
+export default function App() {
+
+  const [submitted, SetSubmitted] = React.useState(false);
+
+  const onPressHandler = () => {
+    SetSubmitted(!submitted)
+    
+  }
+
+    return (
+      <View>
+        <Button 
+            onPress={onPressHandler}
+            title="Submit"
+            color="#579A62"
+        />
+        {submitted?
+            <Text>My username is dumb</Text>
+            
+            
+            :
+            null
+        }
+      </View>
+    )
+}
+*/
+
 export default function App() {
     return (
       <LogIn_Screen/>
     )
 }
 
+//<LogIn_Screen/>
 //export default App;
